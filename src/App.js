1ced92a26d0214bb9f7a41e4c93db0a38de101c5
ashaysk <ashaysk@gmail.com>
2020-10-13 11:34:15 +0530

    Renamed component name and changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Switch } from 'react-router-dom';
 import "./App.css";
-import TestPretty from './components/testPretty';
+import UsersMain from './components/UsersMain';
 import ProductDetail from './components/productdetail';
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/productDetail" exact component={ProductDetail} />
-        <Route path="/" exact component={TestPretty} />
+        <Route path="/" exact component={UsersMain} />
       </Switch>
     </div>
   );
